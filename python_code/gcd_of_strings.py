b97b431c621ac0e7340e768ef2c7d075a2d92c2f
jjwells113 <jjwells113@gmail.com>
2023-05-21 15:48:12 -0500

    Update gcd_of_strings.py

--- python_code/gcd_of_strings.py
+++ python_code/gcd_of_strings.py
@@ -1,3 +1,13 @@
+# 1071. Greatest Common Divisor of Strings
+# Sample Problem from Leetcode
+
+# For two strings s and t, we say "t divides s" if and only if 
+# s = t + ... + t (i.e., t is concatenated with itself one or more times).
+# Given two strings str1 and str2, return the largest string x 
+# such that x divides both str1 and str2.
+
+# Johnathan Wells
+
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         for x in reversed( range(len(str2)) ):
